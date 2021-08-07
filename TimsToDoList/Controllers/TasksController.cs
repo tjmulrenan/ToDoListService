f63758f27b3303c0e320cda0bba6b186adc7f853
unknown <timmulrenan@hotmail.com>
2021-08-07 17:45:23 +0100

    Learning ASP.NET

---
+++ TimsToDoList/Controllers/TasksController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimsToDoList.Services;
+
+namespace TimsToDoList.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("ReactPolicy")]
+    public class TasksController : ControllerBase
+    {
+        private readonly TaskService _taskService;
+        public TasksController(TaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        // GET api/tasks
+        [HttpGet]
+        public IEnumerable<Models.Task> Get()
+        {
+            return (IEnumerable<Models.Task>)_taskService.GetAll;
+        }
+
+        // GET api/tasks/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(_taskService.GetById(id));
+        }
+
+        // POST api/tasks
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Models.Task task)
+        {
+            return CreatedAtAction("Get", new { id = task.Id }, _taskService.Create(task));
+        }
+
+        // POST api/tasks/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Post (int id, [FromBody] Models.Task task)
+        {
+            _taskService.Update(id, task);
+            return NoContent();
+        }
+
+        // DELETE api/tasks/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete (int id)
+        {
+            _taskService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ TimsToDoList/Services/TaskService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using TimsToDoList.Models;
+
+namespace TimsToDoList.Services
+{
+    public class TaskService
+    {
+        private List<Task> _tasks = new List<Task>();
+        private int _count;
+
+        public TaskService()
+        {
+            // Deserialize from text
+            if(File.Exists("TaskData"))
+            {
+                _tasks = Deserialize(File.ReadAllText("TaskData"));              
+            }
+
+            _count = _tasks.Any() ? _tasks.Max(task => task.Id) : 0;
+        }
+
+        private string Serialize(List<Task> t)
+        {
+            var json = JsonSerializer.Serialize(t);
+            return json;
+        }
+
+        private List<Task> Deserialize(string json)
+        {
+            var tasks = JsonSerializer.Deserialize<List<Task>>(json);
+            return tasks;
+        }
+
+        public List<Task> GetAll => _tasks.OrderBy(o=>o.Created).ToList(); //expression bodied member for "() {return tasks}"
+
+        public Task GetById (int id)
+        {
+            return _tasks.FirstOrDefault(task => task.Id == id); //return first item satisfying predicate
+        }
+
+        public Task Create(Task task)
+        {
+            task.Id = ++_count;
+            task.Created = DateTime.Now;
+            _tasks.Add(task);
+            Save();
+            return task;
+        }
+        
+        public void Update (int id, Task task)
+        {
+            Task found = _tasks.FirstOrDefault(task => task.Id == id);
+            found.Name = task.Name;
+            found.Description = task.Description;
+
+            Save();
+        }
+        
+        public void Delete(int id)
+        {
+            _tasks.RemoveAll(task => task.Id == id);
+            Save();
+        }
+
+        private void Save()
+        {
+            File.WriteAllText("TaskData", Serialize(_tasks));
+        }
+    }
+}
